45967e0b7d73ee06b92c233ceb5446b03a708ab9
vasRiddla <65939154+vasRiddla@users.noreply.github.com>
2021-05-19 01:47:26 +0300

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    
+    public class Lab1
+    {
+        private int Counter_CriticalExceptions { get; set; }
+
+        private int Counter_NonCriticalExceptions { get; set; }
+
+        public void ResetCounters()
+        {
+            Counter_CriticalExceptions = 0;
+            Counter_NonCriticalExceptions = 0;
+        }
+
+        public int Get_Counter_Critical_EX() 
+        {
+            return Counter_CriticalExceptions; 
+        }
+
+        public int Get_Counter_NonCritical_EX() 
+        { 
+            return Counter_NonCriticalExceptions;
+        }
+
+        public bool IsCritical(Exception exception)
+        {
+             List<Type> CriticalException = new List<Type>()
+             {
+                typeof(DivideByZeroException),
+                typeof(OutOfMemoryException),
+                typeof(StackOverflowException),
+                typeof(InsufficientMemoryException), // Недостатньо памяті
+                typeof(InsufficientExecutionStackException) // Недостатнє виконання стеку 
+             };
+
+            return CriticalException.Contains(exception.GetType());
+        }
+
+        public void CountExceptions(Exception exception)
+        {
+            if (IsCritical(exception))
+            {
+                Counter_CriticalExceptions += 1;
+            }
+            else
+            {
+                Counter_NonCriticalExceptions += 1;
+            }
+        }
+
+       
+    }
+}
---
+++ Test_testcase (3).cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp1;
+using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class Lab1_Tests
+    {
+        [TestCase(typeof(DivideByZeroException), true)]
+        [TestCase(typeof(StackOverflowException), true)]
+        [TestCase(typeof(OutOfMemoryException), true)]
+        [TestCase(typeof(InsufficientMemoryException), true)]
+        [TestCase(typeof(InsufficientExecutionStackException), true)]
+
+        [TestCase(typeof(ArgumentOutOfRangeException), false)]
+        [TestCase(typeof(NullReferenceException), false)]
+        [TestCase(typeof(ArgumentNullException), false)]
+        [TestCase(typeof(AccessViolationException), false)]
+        [TestCase(typeof(IndexOutOfRangeException), false)]
+
+
+
+        [Test]
+        public void Count_Exceptions_Counter_Values_Correct()
+        {
+
+            List<Type> CriticalExceptions = new List<Type>();
+
+            CriticalExceptions.Add(typeof(DivideByZeroException));
+            CriticalExceptions.Add(typeof(StackOverflowException));
+            CriticalExceptions.Add(typeof(OutOfMemoryException));
+            CriticalExceptions.Add(typeof(InsufficientMemoryException));
+            CriticalExceptions.Add(typeof(InsufficientExecutionStackException));
+
+
+            List<Type> nonCriticalExceptions = new List<Type>();
+
+            nonCriticalExceptions.Add(typeof(ArgumentOutOfRangeException));
+            nonCriticalExceptions.Add(typeof(ArgumentNullException));
+            nonCriticalExceptions.Add(typeof(NullReferenceException));
+            nonCriticalExceptions.Add(typeof(AccessViolationException));
+            nonCriticalExceptions.Add(typeof(IndexOutOfRangeException));
+
+            var lab1 = new Lab1();
+
+            for (int i = 0; i < CriticalExceptions.Count; i++) 
+            {
+                var Exception_ekzemplar = (Exception)Activator.CreateInstance(CriticalExceptions[i]);
+                lab1.CountExceptions(Exception_ekzemplar);
+            }
+
+            for (int i = 0; i < nonCriticalExceptions.Count; i++)
+            {
+                var Exception_ekzemplar = (Exception)Activator.CreateInstance(nonCriticalExceptions[i]);
+                lab1.CountExceptions(Exception_ekzemplar);
+            }
+
+            int CC= lab1.Get_Counter_Critical_EX();
+            int NCC= lab1.Get_Counter_NonCritical_EX();
+            lab1.ResetCounters();
+        }
+
+        [Test]
+        public void Check_To_Correct_Critical_EX(Type Type_exception, bool Result_expected)
+        {
+
+            var ekzemplar = (Exception)Activator.CreateInstance(Type_exception);
+            
+            try
+            {
+                throw ekzemplar;
+            }
+            catch (Exception EX)
+            {
+
+                Assert.AreEqual(Result_expected, new Lab1().IsCritical(EX));
+                return;
+            }
+        }
+
+        [Test]
+        public void Count_Exceptions_Init_Counters_Zero()
+        {
+            
+            var lab1 = new Lab1();
+            lab1.ResetCounters();
+        }
+    }
+}
